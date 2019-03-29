978e946ecdb113e80262f7334549537e97321926
Imane <imane.92@live.fr>
2019-03-29 10:47:31 +0100

    changement dans script

---
+++ main.js
@@ -0,0 +1,12 @@
+function open_fen(){
+	alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please. (Meme si je ne touche que 1$ pour ca ;) )');
+	if(confirm("Voulez-vous aller sur le site ?")){
+		alert("oui");
+		document.location.href=" https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+	}
+	//document.location.href=" https://www.youtube.com/watch?v=dQw4w9WgXcQ"; 
+	else{
+		alert("non");
+	}
+}
+setTimeout(open_fen,10000);
